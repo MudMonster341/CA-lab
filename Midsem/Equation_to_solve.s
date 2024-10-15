6d94ffac99b523dec1c88424d9c3804e0ebebd1b
Mustafa Fatehi <86043055+MudMonster341@users.noreply.github.com>
2024-10-15 19:50:53 +0400

    Add files via upload

---
+++ Midsem/Equation_to_solve.s
@@ -0,0 +1,58 @@
+.data
+msga: .asciiz "the sum is:"
+msgb: .asciiz "enter the value of x:"
+array1: .word 1, 2, 3, 4
+len: .word 4
+
+
+.text
+main:	la $a1,array1	#array1 address loaded 
+	
+	li $v0,4
+	la $a0,msgb
+	syscall		#printing msg to accept x value
+
+	li $v0,5
+	syscall
+	move $s1,$v0	#getting the x value
+
+	move $t1,$zero 	#setting sum to zero
+	la $a1,array1	#loading adress of array
+
+	lw $t2,12($a1)	#loading last element
+	add $t1,$t1,$t2	#last element added A[3]
+	 
+	lw $t2,8($a1)	#loading second last element
+	move $t3,$s1
+	multu $t2,$t3	#A[2] * x
+	mflo $t4
+	add $t1,$t1,$t4	#added to sum
+
+	lw $t2,4($a1)	#loading third last element
+	move $t3,$s1
+	multu	 $t3,$t3
+	mflo $t3	# x*x stored in t3
+
+	multu $t2,$t3	#A[1] * x*x
+	mflo $t4
+	add $t1,$t1,$t4	#added to sum
+
+	lw $t2,0($a1)	#loading fourth last element
+	move $t3,$s1
+	multu $t3,$t3
+	mflo $t3	# x*x*x stored in t3
+
+	multu $t2,$t3	#A[1] * x*x*x
+	mflo $t4
+	add $t1,$t1,$t4	#added to sum
+
+	li $v0,4	#syscall to print msga
+	la $a0,msga	#loading address of msga into a0
+	syscall
+
+	li $v0,1
+	move $a0,$t1	#printing sum 
+	syscall
+
+	li $v0,10
+	syscall 
---
+++ Midsem/multiply_array_and_sum.s
@@ -0,0 +1,40 @@
+.data
+msga: .asciiz "the sum after multiplication is:"
+array1: .word 1, 2, 3
+array2: .word 4, 5, 6
+len: .word 3
+
+
+.text
+main:	lw $t0,len 	#len of array put into t0
+	la $a1,array1	#array1 address loaded 
+	la $a2,array2	#array2 address loaded 
+	
+	move $t3,$zero 	#t3 is the sum
+	
+up:	beq $t0,$zero,exit
+
+	lw $t1,0($a1)	#load value from array 1
+	lw $t2,0($a2)	#load value from array 2
+
+	multu $t1,$t2	#mul stores quotient in mflo and mfhi (here same value)
+	mflo $t4	#store value after multiplication in t4
+
+	add $t3,$t3,$t4	#add to sum
+
+	addi $a1,$a1,4	#next element of array1
+	addi $a2,$a2,4	#next element of array2
+	addi $t0,$t0,-1	#decrement len (counter)	
+
+	j up	 
+
+exit:	li $v0,4	#syscall to print msga
+	la $a0,msga	#loading address of msga into a0
+	syscall
+
+	li $v0,1
+	move $a0,$t3	#printing sum 
+	syscall
+
+	li $v0,10
+	syscall 
